29f86ebde623910d74c2292b61ccfb3a9657e36e
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-05-03 22:35:08 +0530

    correct async-await function which is not work also handle error

--- Making the Registration Page Dynamic/PromiseAll(Task-13)/manalitrip.js
+++ Making the Registration Page Dynamic/PromiseAll(Task-13)/manalitrip.js
@@ -27,7 +27,7 @@ function reachmanali() {
 function goingtiResturnet() {
   return new Promise((resolve, reject) => {
     setTimeout(function () {
-      reject("due to some accident");
+      reject("Error : reject plan due to some accident");
     }, 3000);
   })
 }
@@ -46,21 +46,24 @@ buyCar().then((msg) => {
   })
 })
 
-//by using async-await
-const result = async () => {
-  let msg1 = await buyCar();
-  console.log(msg1);
-  let msg2 = await planatrip();
-  console.log(msg2);
-  let msg3 = await reachmanali();
-  console.log(msg3);
-  let error = await goingtiResturnet();
-  console.log(error);
-}
 
+//or by using async-await
+const result = async () => {
+  try {
+    let msg1 = await buyCar();
+    console.log(msg1);
+    let msg2 = await planatrip();
+    console.log(msg2);
+    let msg3 = await reachmanali();
+    console.log(msg3);
+    let msg4 = await goingtiResturnet();
+  }
+  catch (error) { console.log(error) }
+}
+result();
 /*
 Buy a car
 lets go to manali
 Reach manali
-due to some accident
+Error : reject plan due to some accident
 */
